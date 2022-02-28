7ed7366b0db34074dd412d438b3f82a4cce918a0
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-02-28 23:24:20 +0600

    phone fetched

--- js/app.js
+++ js/app.js
@@ -12,5 +12,20 @@ const displaySearchResult = datas => {
     const searchResult = document.getElementById('search-result');
     datas.forEach(data => {
         console.log(data);
+        const div = document.createElement('div')
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100">
+        <img src="${data.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${data.brand}</h5>
+            <h5 class="card-title">${data.phone_name}</h5>
+        </div>
+        <div class="card-footer">
+        <button class="btn btn-primary" type="button" id="button-search">Details</button>
+        </div>
+         </div>
+        `;
+        searchResult.appendChild(div);
     })
 }
