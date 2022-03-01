c274a68ad09e0dcb40a750f33568b71b23374178
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-03-02 03:22:56 +0600

    bg-color changed

--- js/app.js
+++ js/app.js
@@ -23,14 +23,14 @@ const displaySearchResult = datas => {
         const div = document.createElement('div')
         div.classList.add('col');
         div.innerHTML = `
-        <div class="card p-3 d-flex justify-content-center align-items-center rounded-3">
+        <div class="card p-3 d-flex justify-content-center align-items-center rounded-3 shadow">
         <img src="${data.image}" class="card-img-top img-fluid mx-auto" alt="..." style="width:220px;height:280px;">
         <div class="card-body">
             <h5 class="card-title">${data.brand}</h5>
             <h5 class="card-title">${data.phone_name}</h5>
         </div>
         <div class="">
-        <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-primary" type="button" id="button-search">Details</button>
+        <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-outline-success" type="button" id="button-search">Details</button>
         </div>
          </div>
         `;
@@ -43,7 +43,22 @@ const loadPhoneDetail = phoneId => {
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data))
+        .catch(error => displayBluetooth(error, phoneId))
 }
+
+const displayBluetooth = (error, data) => {
+    data = {
+        others: {
+            Bluetooth: 'No',
+            WLAN: 'No',
+            GPS: 'No',
+            NFC: 'No',
+            Radio: 'No',
+            USB: 'No'
+        }
+    }
+}
+
 
 const displayPhoneDetails = phone => {
     const phoneDetails = document.getElementById('phone-details')
@@ -59,7 +74,7 @@ const displayPhoneDetails = phone => {
         finalRelease = releaseDate;
     }
     div.innerHTML = `
-        <div class="row g-0">
+        <div class="row g-0 shadow">
                 <div class="col-md-4 p-5">
                     <img src="${phone.image}" class="img-fluid mx-auto" alt="...">
                 </div>
@@ -74,8 +89,8 @@ const displayPhoneDetails = phone => {
                     <p class="card-text"><span class="fw-bold">Chipset: </span>${phone.mainFeatures.chipSet}</p>
                     <p class="card-text"><span class="fw-bold">Memory: </span>${phone.mainFeatures.memory}</p>
                     <p class="card-text"><span class="fw-bold">Sensors: </span>${phone.mainFeatures.sensors}</p>
-                    <p class="card-text"><span class="fw-bold">Bluetooth: </span>${phone.others.Bluetooth}</p>
                     <p class="card-text"><span class="fw-bold">Other Features</p>
+                    <p class="card-text"><span class="fw-bold">Bluetooth: </span>${phone.others.Bluetooth}</p>
                     <p class="card-text"><span class="fw-bold">WLAN: </span>${phone.others.WLAN}</p>
                     <p class="card-text"><span class="fw-bold">GPS: </span>${phone.others.GPS}</p>
                     <p class="card-text"><span class="fw-bold">NFC: </span>${phone.others.NFC}</p>
