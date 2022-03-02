bc1157775d7594d07d7db0e3de1a5379ce297e27
Naimur Rahib <61423411+7rahib@users.noreply.github.com>
2022-03-02 20:06:59 +0600

    others property not available error handled

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
 
 
 const searchFood = () => {
+    // added eventlishener to get search input value
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
     searchField.value = "";
-    document.getElementById('error-message').style.display = 'none';
+
+    // hiding error when new search is given
+    document.getElementById('no-input-error-message').style.display = 'none';
+
+    // showing error if no input is given
     if (searchText == '') {
-        document.getElementById('error-message').style.display = 'block';
+        document.getElementById('no-input-error-message').style.display = 'block';
     }
     else {
+
+        // fetching all phones from API
         const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
         fetch(url)
             .then(res => res.json())
@@ -18,53 +25,57 @@ const searchFood = () => {
 const displaySearchResult = datas => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
-    const items = datas.slice(0, 20)
-    items.forEach(data => {
-        const div = document.createElement('div')
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card p-3 d-flex justify-content-center align-items-center rounded-3 shadow">
+
+    // hiding error when new search is given
+    document.getElementById('wrong-input-error-message').style.display = 'none';
+
+    // showing error if no name are matched in the object
+    if (datas.length == 0) {
+        document.getElementById('wrong-input-error-message').style.display = 'block';
+    }
+    else {
+
+        // slicing the object to show only 20 results
+        const items = datas.slice(0, 20)
+        items.forEach(data => {
+            const div = document.createElement('div')
+            div.classList.add('col');
+            // Showing results of search name
+            div.innerHTML = `
+        <div class="card p-3 d-flex justify-content-center align-items-center rounded-3 shadow m-2">
         <img src="${data.image}" class="card-img-top img-fluid mx-auto" alt="..." style="width:220px;height:280px;">
         <div class="card-body">
             <h5 class="card-title">${data.brand}</h5>
             <h5 class="card-title">${data.phone_name}</h5>
         </div>
-        <div class="">
-        <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-outline-success" type="button" id="button-search">Details</button>
+        <div>
+        <button onclick="loadPhoneDetail('${data.slug}')" class="btn btn-danger" type="button" id="button-search">Details</button>
         </div>
          </div>
         `;
-        searchResult.appendChild(div);
-    })
+            searchResult.appendChild(div);
+        })
+    }
 }
 
+// fetching details from API
 const loadPhoneDetail = phoneId => {
     const url = `https://openapi.programming-hero.com/api/phone/${phoneId}`
     fetch(url)
         .then(res => res.json())
         .then(data => displayPhoneDetails(data.data))
-        .catch(error => displayBluetooth(error, phoneId))
 }
 
-const displayBluetooth = (error, data) => {
-    data = {
-        others: {
-            Bluetooth: 'No',
-            WLAN: 'No',
-            GPS: 'No',
-            NFC: 'No',
-            Radio: 'No',
-            USB: 'No'
-        }
-    }
-}
-
-
+// Details section
 const displayPhoneDetails = phone => {
+
+    // adding eventlishener to add a div with class
     const phoneDetails = document.getElementById('phone-details')
     const div = document.createElement('div')
     phoneDetails.textContent = '';
     div.classList.add('card')
+
+    // Checking if Release Date is available or not
     const releaseDate = phone?.releaseDate;
     let finalRelease = '';
     if (releaseDate == '') {
@@ -73,14 +84,18 @@ const displayPhoneDetails = phone => {
     else {
         finalRelease = releaseDate;
     }
+
+    // Checking if others property is available or not
     const others = phone?.others;
-    let avaiOthers = ''
+    let avaiOthers = [];
     if (others) {
         avaiOthers = others;
     }
     else {
-        avaiOthers = 'Not Available'
+        avaiOthers = phone.others = { WLAN: 'Not data available', Bluetooth: 'Not data available', GPS: 'Not data available', NFC: 'Not data available', Radio: 'Not data available', USB: 'Not data available' }
     }
+
+    // Showing the details section
     div.innerHTML = `
         <div class="row g-0 shadow">
                 <div class="col-md-4 p-5">
@@ -110,4 +125,6 @@ const displayPhoneDetails = phone => {
         `
 
     phoneDetails.appendChild(div)
+    // window scroll to top when clicked
+    window.scrollTo(0, 0);
 }
